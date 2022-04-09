8ea7231575d7d55d44fbe434753767ad715c89c7
Jose Vicente <velasco.josevicente @gmail.com>
2022-04-09 18:44:58 -0500

    Se configura el log4j

--- src/test/java/co/com/sofka/stepdefinitions/SetupWebUI.java
+++ src/test/java/co/com/sofka/stepdefinitions/SetupWebUI.java
@@ -1,9 +1,11 @@
 package co.com.sofka.stepdefinitions;
 
+import co.com.sofka.util.Log4jValues;
 import net.serenitybdd.screenplay.abilities.BrowseTheWeb;
 import net.serenitybdd.screenplay.actors.OnStage;
 import net.serenitybdd.screenplay.actors.OnlineCast;
 import net.thucydides.core.annotations.Managed;
+import org.apache.log4j.PropertyConfigurator;
 import org.openqa.selenium.WebDriver;
 
 
@@ -25,8 +27,13 @@ public class SetupWebUI {
     }
 
     protected void actorSetupTheBrowser(String actorName){
+        setUpLog4j2();
         setupBrowser(browser);
         setupUser(actorName, browser);
+    }
+
+    private void setUpLog4j2(){
+        PropertyConfigurator.configure(Log4jValues.USER_DIR.getValue()  + Log4jValues.LOG4J_PROPERTIES_FILE_PATH.getValue());
     }
 
 }
